ca0529f502746f788581a61c94bc51527cd8889c
neuroph <sevarac@gmail.com>
2016-12-10 23:54:05 +0100

    classifier & image recognition

--- VisualRecognitionAPI/src/main/java/visrec/samples/ImageRecognitionSample.java
+++ VisualRecognitionAPI/src/main/java/visrec/samples/ImageRecognitionSample.java
@@ -2,6 +2,7 @@ package visrec.samples;
 
 import java.awt.image.BufferedImage;
 import visrec.classifier.Classifier;
+import visrec.classifier.SimpleClassifier;
 import visrec.tasks.ImageRecognition;
 import visrec.tasks.ImageRecognitionProvider;
 
@@ -12,7 +13,8 @@ import visrec.tasks.ImageRecognitionProvider;
 public class ImageRecognitionSample {
 
     public static void main(String[] args) {        
-        Classifier<BufferedImage, String> someClassifier;
+        Classifier<BufferedImage, String> someClassifier = new SimpleClassifier();
+        // someClassifier.buildClassifier(data);
         
         ImageRecognition<BufferedImage, String> ir = new ImageRecognitionProvider<BufferedImage, String>(someClassifier);
         ir.recognizeImage((BufferedImage) ir);
