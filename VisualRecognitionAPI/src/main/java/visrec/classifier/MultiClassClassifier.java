8053e3741cd5231e014e3740acfe332d6f7a6507
zoran <zoran@zoran-Inspiron>
2017-10-31 22:12:04 +0100

    Latest visrec

--- VisualRecognitionAPI/src/main/java/visrec/classifier/MultiClassClassifier.java
+++
@@ -1,9 +0,0 @@
-package visrec.classifier;
-
-/**
- *
- * @author Zoran Sevarac <zoran.sevarac@deepnetts.com>
- */
-public interface MultiClassClassifier<T>  extends Classifier<T, String> {
-    
-}
