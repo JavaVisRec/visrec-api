3c094cb15a2c3bfdfe4d973db0d90a40865293c5
Zoran <Zoran@LAPTOP-VDMLK9JK>
2018-04-08 16:45:29 +0200

    javadoc update

---
+++ VisualRecognitionApi/src/main/java/javax/visrec/package-info.java
@@ -0,0 +1,5 @@
+/*
+ * Visual recognition API provides abstractions for visual recognition tasks based on machine learning,
+ * and enables simple integratin of different machine learning models and image types.
+ */
+package javax.visrec;
