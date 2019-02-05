24f2b02481262a4606f0a9797231a4fafc2f6a30
Kevin Berendsen <info@kevinberendsen.nl>
2019-02-05 10:03:20 +0100

    Fixing JavaDoc of Builder utility class.

--- src/main/java/javax/visrec/util/Builder.java
+++ src/main/java/javax/visrec/util/Builder.java
@@ -15,16 +15,16 @@ public interface Builder<T> {
     /**
      * Builds and returns an object using properties set using available builder methods.
      * 
-     * @return 
+     * @return object specified by the builder to build
      */
-    public T build();
+    T build();
     
     /**
      * Builds an object using properties from the specified input argument
      * 
-     * @param prop
-     * @return 
+     * @param prop configuration properties for the builder.
+     * @return object specified by the builder to build
      */
-    public T build(Properties prop); // set everything from configuration properties and then build and return T instance. Make this default method and use keys as method names?
+    T build(Properties prop); // set everything from configuration properties and then build and return T instance. Make this default method and use keys as method names?
   
 }
