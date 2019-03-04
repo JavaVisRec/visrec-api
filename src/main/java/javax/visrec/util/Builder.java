233dc825d90370f786452f3f74973aacd507e258
Kevin Berendsen <info@kevinberendsen.nl>
2019-03-04 16:17:23 +0100

    Updated Builder to apply default configuration of the implementation if the build() method is used without additional configuration

--- src/main/java/javax/visrec/util/Builder.java
+++ src/main/java/javax/visrec/util/Builder.java
@@ -1,5 +1,6 @@
 package javax.visrec.util;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -12,11 +13,21 @@ import java.util.Map;
 public interface Builder<T> {
 
     /**
+     * Returns the default configuration for the builder. By default it is returning an empty {@code HashMap}
+     * @return {@code Map} of {@code String} as key and {@code Object} as value.
+     */
+    default Map<String, Object> defaultConfiguration() {
+        return new HashMap<>();
+    }
+
+    /**
      * Builds and returns an object using properties set using available builder methods.
      *
      * @return object specified by the builder to build
      */
-    T build();
+    default T build() {
+        return build(defaultConfiguration());
+    }
 
     /**
      * Builds an object using properties from the specified input argument
