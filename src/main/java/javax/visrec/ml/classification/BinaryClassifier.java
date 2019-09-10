59ec2d583abafd2009c084a150d6925ba2dcfe68
Zoran Sevarac <sevarac@gmail.com>
2019-09-10 15:29:32 +0200

    minor polishing

--- src/main/java/javax/visrec/ml/classification/BinaryClassifier.java
+++ src/main/java/javax/visrec/ml/classification/BinaryClassifier.java
@@ -1,12 +1,13 @@
 package javax.visrec.ml.classification;
 
 /**
- * Binary classifier classifies object into one of two categories (for example: true/false, yes/no, red/blue, spam/not-spam, fraud/not-fraud)
+ * Binary classifier classifies object into one of two categories (for example: true/false, yes/no, red/blue, spam/not-spam, fraud/not-fraud).
+ * Returns a probability that input object belongs to one of two classes.
  * 
- * @author Zoran
+ * @author Zoran Sevarac
  * @param <T> type of object to classify
  */
 public interface BinaryClassifier<T> extends Classifier<T, Float> {
-    
+   
     
 }
