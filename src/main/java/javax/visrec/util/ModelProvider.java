85feaf85fb274127763071222c4d3449da97713a
Zoran Sevarac <sevarac@gmail.com>
2020-07-14 15:28:16 +0200

    Renaming type param to conform convention in ModelProvider

--- src/main/java/javax/visrec/util/ModelProvider.java
+++ src/main/java/javax/visrec/util/ModelProvider.java
@@ -1,14 +1,12 @@
 package javax.visrec.util;
 
 /**
- * This interface should be implemented if we want to allow access to underlying ML model
- * @author Zoran
- * @param <M> 
+ * This interface should be implemented by classes which expose access to underlying ML model
+ * 
+ * @param <T> Type of underlying ML model
  */
-public interface ModelProvider<M> {
+public interface ModelProvider<T> {
     
-    M getModel();
-   
-    // also provide public setModel to allow model injection?
+    T getModel();
    
 }
