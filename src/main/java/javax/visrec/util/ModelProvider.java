31b7e42d2fc50f83f584664145d59dab5cd7ec2d
Zoran Sevarac <sevarac@gmail.com>
2020-07-14 15:29:12 +0200

    javadoc fixes

--- src/main/java/javax/visrec/util/ModelProvider.java
+++ src/main/java/javax/visrec/util/ModelProvider.java
@@ -3,7 +3,7 @@ package javax.visrec.util;
 /**
  * This interface should be implemented by classes which expose access to underlying ML model
  * 
- * @param <T> Type of underlying ML model
+ * @param <T> Type of the underlying ML model
  */
 public interface ModelProvider<T> {
     
