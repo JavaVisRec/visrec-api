1b06cb6d2ab42bd215cd45373415582b6c7bee57
deepnetts <Zoran@LAPTOP-VDMLK9JK>
2021-12-02 13:15:04 +0100

    Added GPL header to all files

--- src/main/java/javax/visrec/ml/classification/Classifiable.java
+++ src/main/java/javax/visrec/ml/classification/Classifiable.java
@@ -1,3 +1,22 @@
+/**
+ * Visual Recognition API for Java, JSR381
+ * Copyright (C) 2020  Zoran Sevarac, Frank Greco
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
 package javax.visrec.ml.classification;
 
 /**
@@ -10,7 +29,7 @@ package javax.visrec.ml.classification;
  * to build machine learning based classifiers.
  * Typical implementation scenario is to wrap domain specific class and implement this interface.
  * 
- * Classifiable<T, C> can be classfied by the Classifier<T, C> (with the same T and C)
+ * Classifiable<T, C> can be classified by the Classifier<T, C> (with the same T and C)
  * 
  * @param <T> Type of input for classifier
  * @param <C> Type of categories/labels (could be anything like enum, String, Integer or user defined class)
